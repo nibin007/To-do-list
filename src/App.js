835c6793365e9ab8221032165a7d29383db003af
Nibin <nibin98prasad@gmail.com>
2021-11-16 19:26:19 +0530

    todo commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[toDos,settoDos]=useState([])
+  const[Todo,setTOdo]=useState('')
+  return ( <div className="app">
+  <div className="mainHeading">
+    <h1>ToDo List</h1>
+  </div>
+  <div className="subHeading">
+    <br />
+    <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+  </div>
+  <div className="input">
+    <input value={Todo} onChange={(e)=>{setTOdo(e.target.value)}} type="text" placeholder="🖊️ Add item..." />
+    <i onClick={()=>{settoDos([...toDos,{id:Date.now(),text:Todo,status2:false,status:false}])}} className="fas fa-plus"></i>
+  </div>
+  
+  <div className="todos">
+    { toDos.map((obj=>{
+      if(obj.status2===false){ 
+    return(
+    <div className="todo">
+      <div className="left">
+        <input onChange={(e)=>{
+          settoDos(toDos.filter(obj2=>{
+            console.log(e.target.checked)
+             if(obj2.id===obj.id){
+              obj2.status=e.target.checked
+            }
+            return obj2
+          }))
+        }} type="checkbox" name="" id="" />
+        <p>{obj.text} </p> 
+      </div>
+      <div className="right">
+        <i onClick={()=>{settoDos(toDos.filter(obj3=>{
+          if(obj3.id===obj.id){
+            obj3.status2=true
+          }
+          return obj3
+        }))}} className="fas fa-times"></i>
+      </div>
+    </div>)}
+     return null
+  }))}
+    { toDos.map((obj)=>{
+      if(obj.status&&obj.status2===false){
+        return(<h1>{obj.text}</h1>)
+      }
+      return null
+    })
+
+    }
+    { toDos.map((obj)=>{
+      if(obj.status2===true){
+        return(
+        <h1>{obj.text} </h1> 
+          )
+      }return null
+    })}
+  </div>
+</div>
+    
   );
 }
 
